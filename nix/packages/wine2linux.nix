2ee66529f9909eb81b045d146f437a709ce9c152
Samuel Rounce <me@samuelrounce.co.uk>
2026-05-05 19:15:48 +0100

    chore: Add Nix derivations

---
+++ nix/packages/wine2linux.nix
@@ -0,0 +1,30 @@
+{ pkgs, system, flake, ... }:
+let
+  mingw = pkgs.pkgsCross.mingwW64;
+  rustToolchain = flake.lib.mkRustToolchain system;
+  rustPlatform = mingw.makeRustPlatform {
+    cargo = rustToolchain;
+    rustc = rustToolchain;
+  };
+  cargoToml = builtins.fromTOML (builtins.readFile ../../Cargo.toml);
+  target = "x86_64-pc-windows-gnu";
+in
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "wine2linux";
+  version = cargoToml.workspace.package.version;
+  src = ../..;
+  cargoBuildFlags = [ "--package" finalAttrs.pname ];
+  cargoTestFlags = [ "--package" finalAttrs.pname ];
+  cargoLock.lockFile = ../../Cargo.lock;
+
+  buildInputs = [ mingw.windows.pthreads ];
+
+  doCheck = false;
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/bin
+    install -m 755 target/${target}/release/wine2linux.exe $out/bin/
+    runHook postInstall
+  '';
+})
